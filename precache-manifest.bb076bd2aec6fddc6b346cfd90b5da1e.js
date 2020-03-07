7daf925372d381e8e28a3e046b0b35d63e731da1
Asad Saleem <aasad48942@gmail.com>
2020-03-07 18:27:48 +0500

    Updates

---
+++ precache-manifest.bb076bd2aec6fddc6b346cfd90b5da1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50500df68cd7312714fddb87d8eb0ca9",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/index.html"
+  },
+  {
+    "revision": "aa1bd8a023ba5a92b678",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "46c3edeaca41a71ba400",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "aa1bd8a023ba5a92b678",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/static/js/2.bc489580.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/static/js/2.bc489580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c3edeaca41a71ba400",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/static/js/main.a810ca5f.chunk.js"
+  },
+  {
+    "revision": "cedb817fe5c520f70de9",
+    "url": "/github.io/Contact-Manager-with-fake-rest-API-/static/js/runtime-main.82a9cfd5.js"
+  }
+]);
